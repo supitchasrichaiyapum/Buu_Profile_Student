c9af6edc4ac6b00198613b59ab250ce4a1ee7112
Swiss Sawasdee <57660136@go.buu.ac.th>
2018-02-28 19:20:17 +0700

    comment

--- application/views/admin/addaward_student_admin.php
+++ application/views/admin/addaward_student_admin.php
@@ -14,45 +14,58 @@
             </div>
 
            <div class="card-body">
+           
                 <form action="" method="post" enctype="multipart/form-data" class="form-horizontal">
                     <div class="form-group row">
                         <label class="col-md-3 col-form-label" for="text-input">ปีการศึกษา</label>
                             <div class="col-md-3">
-                                <input type="text" id="text-input" name="text-year" class="form-control">
+                                <input type="text" id="text-input" name="text-year" class="form-control" required="">
                             </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-md-3 col-form-label" for="text-input">ชื่อ - นามสกุล (นิสิต)</label>
+                        <label class="col-md-3 col-form-label" for="text-input">รหัสนิสิต</label>
                             <div class="col-md-3">
-                                <input type="text" id="text-input" name="text-name" class="form-control">
+                                <input type="text" id="text-input" name="text-name" class="form-control" required="">
+                            </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-3 col-form-label" for="text-input">ชื่อ</label>
+                            <div class="col-md-3">
+                                <input type="text" id="text-input" name="text-name" class="form-control" required="">
+                            </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-3 col-form-label" for="text-input">นามสกุล</label>
+                            <div class="col-md-3">
+                                <input type="text" id="text-input" name="text-name" class="form-control" required="">
                             </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-3 col-form-label" for="text-input">หลักสูตร</label>
                             <div class="col-md-3">
                                 <select id="select1" name="select1" class="form-control">
-                                    <option value="SE">วิศวกรรมซอฟต์แวร์</option>
-                                    <option value="CS">วิทยาการคอมพิวเตอร์</option>
-                                    <option value="IT">เทคโนโลยีสารสนเทศ</option>
+                                    <option value="วิศวกรรมซอฟต์แวร์">วิศวกรรมซอฟต์แวร์</option>
+                                    <option value="วิทยาการคอมพิวเตอร์">วิทยาการคอมพิวเตอร์</option>
+                                    <option value="เทคโนโลยีสารสนเทศ">เทคโนโลยีสารสนเทศ</option>
                                 </select>
                             </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-3 col-form-label" for="text-input">อาจารย์ผู้ช่วย</label>
                             <div class="col-md-3">
-                                <input type="text" id="text-input" name="text-input" class="form-control">
+                                <input type="text" id="text-input" name="text-input" class="form-control" required="">
                             </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-3 col-form-label" for="text-input">ชื่อผลงาน</label>
                             <div class="col-md-3">
-                                <input type="text" id="text-input" name="text-input" class="form-control">
+                                <input type="text" id="text-input" name="text-input" class="form-control" required="">
                             </div>
                         </div>
                     <div class="form-group row">
                         <label class="col-md-3 col-form-label" for="text-input">วัน / เดือน / ปี</label>
                             <div class="col-md-3">
-                                <input type="date" id="text-input" name="text-input" class="form-control">
+                                <input type="date" id="text-input" name="text-input" class="form-control" required="">
                             </div>
                     </div>
                     <button type="submit" class="btn btn-sm btn-success"> ตกลง </button>
--- application/views/user/activity.php
+++ application/views/user/activity.php
@@ -1,71 +1,69 @@
-<Body>
+<style>
+table {
+    width:60%;
+}
+th, td {
+    padding: 5px;
+    text-align: left;
+}
+      </style>
+  
         <div class="content-inner">
           <!-- Page Header-->
           <header class="page-header">
             <div class="container-fluid">
+              <h1 class="no-margin-bottom"><center>ข้อมูลกิจกรรม</center></h1>
             </div>
           </header>
-          <br>
-        <Body>
-        <div class="col-sm-12">
-          <div class="card">
-            <div class="card-header">
-              <strong><center>ข้อมูลกิจกรรม</strong>
-            </div>
-            <div class="card-body">
 
-        <form  method="get"  action="activity">
-          <div class="container">
-            <div class="row">
-              <div class="col-md-6"><BR>
-                <h2>การตรวจสอบชั่วโมงกิจกรรม</h2><BR>
-                  <div id="custom-search-input">
-                    <div class="input-group col-md-7">
-                      <input type="text" name="textfield" id="textfield" class="form-control input-lg" placeholder="ค้นหารหัสนิสิต" maxlength ="8">&nbsp;&nbsp;
-                      <input type="submit" name="button" id="button" value="ตกลง" class="btn btn-primary">
-                    </div>
+          <section class="dashboard-counts no-padding-bottom">
+            <div class="container-fluid">
+              <div class="row bg-white has-shadow">
+                <!-- Item -->
+                <div class="container">
+                  
+
+<table id="datatable" class="table table-hover table-bordered table-striped">
+<thead>
+            <tr>
+                <th>ปีการศึกษา</th>
+                <th>เทอม</th>
+                <th>ชื่อกิจกรรม</th>
+                <th>รหัสนิสิต</th>
+                <th>ชื่อ - นามสกุล</th>
+                <th>ชั่วโมง</th>
+            </tr>
+</thead>
+<tbody>
+<?php foreach ($query as $row) { ?>
+
+
+<tr>
+
+<td><?php echo $row['Activity_Year'] ?> </td>
+<td><?php echo $row['Activity_Term'] ?> </td>
+<td><?php echo $row['Activitie_Name'] ?> </td>
+<td><?php echo $row['Student_Student_ID'] ?> </td>
+<td><?php echo $row['Student_NameTH'] ?> </td>
+<td><?php echo $row['Student_LNameTH'] ?> </td>
+<td><?php echo $row['Hour'] ?> </td>
+
+</tr>      
+
+
+<?php } ?>
+</tbody>
+</table>             
+
+  
                   </div>
                 </div>
+              </div>
             </div>
-          </div>
-        </form> <BR>
-        <table id="example" class="table table-striped table-bordered datatable" cellspacing="0" width="100%">
-                      <?php
-                        $host = "103.86.50.206";
-                        $username = "buu_profile";
-                        $password = "buu999";
-                        $db = "profile_db";
+          </section>           
 
-                        $conn = new mysqli($host, $username, $password, $db);
-                        $conn->query("set names utf8");
-
-                        if(isset($_GET['button'])){
-                        $searchall = $_GET['textfield'];
-
-                        $sql = "SELECT * 
-                            FROM Activity 
-                            WHERE Student_Code like\"".$searchall."\"";
-
-                        $result = $conn->query($sql);
-                        if($result->num_rows ==0) {
-
-                          echo "ไม่มีข้อมูล";
-
-                        } else {
-                          echo "<TR>";
-                          echo "<TD><center>ชื่อกิจกรรม</TD>";
-                          echo "<TD><center>ชั่วโมง</TD>";
-                          echo "</TR>";
-
-                          while($row = $result->fetch_object()) {
-                            echo "<TR>";
-                            echo "<TD><center>$row->Activitie_Name</TD>";
-                            echo "<TD><center>$row->Hour</TD>";
-                            echo "</TR>";
-
-                          }
-                        }
-                      }
-                      ?>
-                  </table>  
-</Body>
+    <script>
+      $(document).ready(function() {
+          $('#datatable').DataTable();
+      } );
+    </script>
